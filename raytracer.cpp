a372192237401589a3ac0256a040caf119e5dcf5
mbc <malcolmbalch@gmail.com>
2018-08-13 19:31:27 -0400

    Fix segfault, metal works now

--- raytracer.cpp
+++ raytracer.cpp
@@ -5,27 +5,21 @@
 #include "hitable_list.h"
 #include "float.h"
 #include "camera.h"
+#include "material.h"
 
-// TODO: No longer used - in sphere class
-float hit_sphere(const vec3& center, float radius, const ray& r) {
-  vec3 oc = r.origin() - center;
-  float a = dot(r.direction(), r.direction());
-  float b = 2.0 * dot(oc, r.direction());
-  float c = dot(oc, oc) - radius * radius;
-  float discriminant = b*b - 4*a*c;
-  if (discriminant < 0) {
-    return -1.0;
-  } else {
-    return (-b - sqrt(discriminant)) / (2.0*a);
-  }
-  return (discriminant > 0);
-}
 
-vec3 color(const ray& r, hitable *world) {
+vec3 color(const ray& r, hitable *world, int depth) {
+  // Contains the point of the hit, and the normal of the sphere (?)
   hit_record rec;
-  if (world->hit(r, 0.0, MAXFLOAT, rec)) {
-    // This uses the normal of the sphere it's hitting to map from x/y/z to r/g/b for a simple gradient coloring
-    return 0.5*vec3(rec.normal.x()+1, rec.normal.y()+1, rec.normal.z()+1);
+  if (world->hit(r, 0.001, MAXFLOAT, rec)) {
+    ray scattered;
+    vec3 attenuation;
+
+    if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
+      return attenuation*color(scattered, world, depth + 1);
+    } else {
+      return vec3(0, 0, 0);
+    }
   } else {
     // Otherwise, map the pixels color to the background gradient
     vec3 unit_direction = unit_vector(r.direction());
@@ -41,28 +35,38 @@ int main() {
 
   // Number of samples for anti-aliasing
   int ns = 100;
+
   std::cout << "P3\n" << nx << " " << ny << "\n255\n";
-  hitable *list[2];
-  list[0] = new sphere(vec3(0,0,-1), 0.5);
-  list[1] = new sphere(vec3(0, -100.5, -1), 100);
-  hitable *world = new hitable_list(list, 2);
+  hitable *list[4];
+  list[0] = new sphere(vec3(0,0,-1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
+  list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
+  list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2)));
+  list[3] = new sphere(vec3(-1, 0, -1), 0.5, new metal(vec3(0.8, 0.8, 0.8)));
+  hitable *world = new hitable_list(list, 4);
   camera cam;
 
   for (int j = ny-1; j >= 0; j--) {
     for (int i = 0; i < nx; i++) {
       vec3 col(0, 0, 0);
+      // For each pixel, generate a bunch of rays and take the sum of their colors
+      // as the pixel's color. note that drand48() returns a value between 0 and 1
+      // so the anti-aliasing is local to the pixel we're targeting
       for (int s=0; s< ns; s++) {
         float u = float(i + drand48()) / float(nx);
         float v = float(j + drand48()) / float(ny);
         ray r = cam.get_ray(u, v);
         vec3 p = r.point_at_parameter(2.0);
-        col += color(r, world);
+        col += color(r, world, 0);
       }
+      // Average the sum of the colors by the number of samples
       col /= float(ns);
+
+      // I have no idea why we have to "gamma correct" this but here we are
+      col = vec3( sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
       int ir = int(255.99*col[0]);
       int ig = int(255.99*col[1]);
       int ib = int(255.99*col[2]);
       std::cout << ir << " " << ig << " " << ib << "\n";
     }
   }
-}
+};
